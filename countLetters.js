7ec0f4110c275756cab35f9ac269d421caf42af6
aal-brown <adlai.brown@gmail.com>
2020-01-08 21:16:02 +0000

    Function to count the number of each character in a string and return the counts of each.

---
+++ countLetters.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`Assertion Passed 👍️👍️👍️: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed 👎️: ${actual} !== ${expected}`);
+  }
+
+};
+
+// PSeudocode
+// Look through an array, count the number of times each letter appears
+//Take in a string
+//Function that takes in a string,
+//Remove spaces from the string and convert all to lowercase
+//Initialize an object
+//
+//Loop through the string and for each unique character define a new key, if that key already exists then count up, if not create a new key.
+
+let countLetters = function(stringInput) {
+  
+  let cleanString = stringInput.toLowerCase().split(" ").join("");
+  
+  let countedLetters = {};
+
+  for (let char of cleanString) {
+    if (countedLetters[char]) {
+      countedLetters[char] += 1;
+    } else {
+      countedLetters[char] = 1;
+    }
+  }
+  return countedLetters;
+};
+
+console.log(countLetters("lighthouse in the house"));
