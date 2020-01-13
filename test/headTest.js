66b0a5f8139f89daffeca0207fcbaf127d23bf4b
aal-brown <adlai.brown@gmail.com>
2020-01-13 20:56:54 +0000

    Modified the file to run tests with Mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,22 @@
 const head = require('../head.js');
 const assertEqual = require("../assertEqual.js");
+const assert = require('chai').assert;
 
-///Test cases
-console.log(head([]));
-assertEqual(head([1,2,3,4]),1);
-assertEqual(head([1]),1);
-assertEqual(head(["a","b","c"]),"a");
-assertEqual(head(["a","b","c"]),"b");
+////Test cases
+// console.log(head([]));
+// assertEqual(head([1,2,3,4]),1);
+// assertEqual(head([1]),1);
+// assertEqual(head(["a","b","c"]),"a");
+// assertEqual(head(["a","b","c"]),"b");
 
+describe("#head" , () => {
+  it("returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 5 for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns Hello for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
